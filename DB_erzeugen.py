778db83486e333a6409278e53dcfd878bdb97d97
flexdigit <georg-org@hotmail.de>
2015-01-11 20:00:48 +0100

    Update DB_erzeugen.py

--- DB_erzeugen.py
+++ DB_erzeugen.py
@@ -1,63 +1,69 @@
 #!/usr/bin/python
-# -*- coding: utf-8 -*-
-import sqlite3, random, datetime
 
-# Existenz feststellen
-#if os.path.exists("Gaszaehler.db"):
-#    print "File already exist!"
-#    sys.exit(0)
+import sqlite3, datetime, time, os
+
+# If already exist, rename it
+if os.path.exists("Gasmeter.db"):
+    print "File already exist! Change name to Gasmeter.db_OLD"
+    os.rename("Gasmeter.db", "Gasmeter.db_OLD")
+
+#--------------------------------------------------------------------
+# New database Gasmeter.db should contain two tables with the following
+# columns:
+#   - dailyamount     (amount, tstamp)
+#   - gascounter      (tick, tstamp)
+#--------------------------------------------------------------------
 
 # Startwert
 Startwert = 12345.67890
 
+#--------------------------------------------------------------------
+# Calculate yesterday
+#
 today = datetime.datetime.today()
 yesterday = today - datetime.timedelta(days=1)
 yesterday = yesterday.strftime('%Y-%m-%d %H:%M:%S')
 
-def make_entry(table, spalte):
-    sql = 'INSERT INTO %s (%s) VALUES(?)'%(table, spalte)
-    x = random.randint(1,500)
-    args = (x,)
-    cursor.execute(sql, args)
-
-# initialize random
-random.seed()
-
-# Verbindung zur Datenbank erzeugen
-connection = sqlite3.connect("Gaszaehler.db")
-
-# Datensatzcursor erzeugen
+# connect database and create record cursor
+connection = sqlite3.connect("Gasmeter.db")
 cursor = connection.cursor()
 
-# Tabelle erzeugen für Zählerstartwert eintragen
-sql = "CREATE TABLE adjust (counterstart FLOAT)"
+#--------------------------------------------------------------------
+# create table gascounter and put two test values into
+#
+sql = """CREATE TABLE gascounter (tick INTEGER,
+                                  tstamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL)"""
 cursor.execute(sql)
-sql = 'INSERT INTO adjust (counterstart) VALUES(?)'
-args = (Startwert,)
+
+sql = 'INSERT INTO gascounter (tick,tstamp) VALUES(?,?)'
+args = (666,time.strftime("%Y-%m-%d %H:%M:%S"))
+cursor.execute(sql, args)
+sql = 'INSERT INTO gascounter (tick,tstamp) VALUES(?,?)'
+args = (777,time.strftime("%Y-%m-%d %H:%M:%S"))
 cursor.execute(sql, args)
 
-# Tabelle erzeugen
-sql = "CREATE TABLE gascounter (delta INTEGER, time DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL)"
+#--------------------------------------------------------------------
+# create table dailyamount and put two test values into
+#
+sql = """CREATE TABLE dailyamount(amount FLOAT,
+                                  tstamp DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL)"""
 cursor.execute(sql)
 
-# und Testwerte eintragen
-for i in range(10):
-    make_entry('gascounter', 'delta')
-
-# Tabelle erzeugen
-sql = "CREATE TABLE dailyamount(amount FLOAT, time DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL)"
-cursor.execute(sql)
-
-# und Testwerte eintragen
-for i in range(10):
-    make_entry('dailyamount', 'amount')
-
-# Zählerstartwert setzen    
-sql = 'INSERT INTO dailyamount (amount, time) VALUES(?,?)'
-args = ('99099',yesterday)
+sql = 'INSERT INTO dailyamount (amount,tstamp) VALUES(?,?)'
+args = (888,time.strftime("%Y-%m-%d %H:%M:%S"))
+cursor.execute(sql, args)
+sql = 'INSERT INTO dailyamount (amount,tstamp) VALUES(?,?)'
+args = (999,time.strftime("%Y-%m-%d %H:%M:%S"))
 cursor.execute(sql, args)
 
-    
+#--------------------------------------------------------------------
+# Wert setzen und Datum von gestern eintragen
+#
+#sql = 'INSERT INTO dailyamount (amount, time) VALUES(?,?)'
+#args = ('99099',yesterday)
+#cursor.execute(sql, args)
+
+
 # never forget this, if you want the changes to be saved
 connection.commit()
 # Verbindung beenden
